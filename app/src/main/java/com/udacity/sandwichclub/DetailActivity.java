5fbcaaf82689cd44eb3ad9c5bd24159667c95064
Kerry <heyfidler@gmail.com>
2019-04-25 08:13:10 -0700

    Parse JSON and display formatted detail

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,16 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -48,7 +52,32 @@ public class DetailActivity extends AppCompatActivity {
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
 
+        // populate AlsoKnownAs text view
+        TextView alsoKnownTV = findViewById(R.id.also_known_tv);
+        alsoKnownTV.setText(listToCommaString(sandwich.getAlsoKnownAs()));
+
+        // populate Ingredients text view
+        TextView ingredientsTV = findViewById(R.id.ingredients_tv);
+        ingredientsTV.setText(listToCommaString(sandwich.getIngredients()));
+
+        // populate Description text view
+        TextView descriptionTV = findViewById(R.id.description_tv);
+        descriptionTV.setText(sandwich.getDescription());
+
+        // populate Origin text view
+        TextView originTV = findViewById(R.id.origin_tv);
+        originTV.setText(sandwich.getPlaceOfOrigin());
+
         setTitle(sandwich.getMainName());
+    }
+
+    private String listToCommaString(List<String> list) {
+        StringBuilder builder = new StringBuilder();
+        for (String li:list) {
+            builder.append(li).append(", ");
+        }
+        String s = builder.toString();
+        return s.replaceAll(", $", "");
     }
 
     private void closeOnError() {
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,62 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private static final String NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String DESCRIPTION = "description";
+    private static final String IMAGE = "image";
+    private static final String INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        if (json == null) {
+            return null;
+        }
+
+        Sandwich sandwich = new Sandwich();
+
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+
+            if (sandwichJson != null) {
+                JSONObject name = sandwichJson.getJSONObject(NAME);
+                if (name != null) {
+                    sandwich.setMainName(name.getString(MAIN_NAME));
+                    sandwich.setAlsoKnownAs(jsonArrayToList(name.getJSONArray(ALSO_KNOWN_AS)));
+                }
+
+                sandwich.setPlaceOfOrigin(sandwichJson.getString(PLACE_OF_ORIGIN));
+                sandwich.setDescription(sandwichJson.getString(DESCRIPTION));
+                sandwich.setImage(sandwichJson.getString(IMAGE));
+                sandwich.setIngredients(jsonArrayToList(sandwichJson.getJSONArray(INGREDIENTS)));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(JsonUtils.class.getName(), "returning: "  + sandwich.getMainName());
+        return sandwich;
+    }
+
+    private static List<String> jsonArrayToList(JSONArray jsonArray) throws JSONException {
+        List<String> list = new ArrayList<String>();
+        for(int i = 0; i < jsonArray.length(); i++){
+            list.add(jsonArray.getString(i));
+        }
+        return list;
     }
 }
